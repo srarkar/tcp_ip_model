4e1f8d2c8962f2b50b7a77bfcbc1c0e7b4c1024f
srarkar <rickdsarkar@gmail.com>
2025-07-14 13:19:03 -0400

    basic os check and interface pritn

--- data_link/arp_sniffer.py
+++ data_link/arp_sniffer.py
@@ -1,2 +1,21 @@
 # looks at ARP packets sent on LAN, check MAC/IP pairs
 # how to utilize ethernet frame structure??
+
+import netifaces
+import platform
+
+interfaces = netifaces.interfaces()
+
+os = platform.uname().system
+match os:
+    case "Darwin":
+        print("MacOS")
+        # Use "en0" for Ethernet/WiFi frames
+    case "Linux":
+        print("Linux")
+        # Use "eth*" for ethernet and "wlan*" for WiFi
+    case "Windows":
+        print("Windows")
+        # Npcap?
+    case _:
+        raise NotImplementedError("Unsupported or unknown operating system.")
