9c489188445f98b238e68bc8bb4b1184d1fe40f7
srarkar <rickdsarkar@gmail.com>
2025-07-15 15:59:32 -0400

    scapy sniffing

--- data_link/arp_sniffer.py
+++ data_link/arp_sniffer.py
@@ -2,7 +2,10 @@
 # how to utilize ethernet frame structure??
 
 import netifaces
+import scapy.all as scapy
 import platform
+import time
+
 
 interfaces = netifaces.interfaces()
 
@@ -19,3 +22,18 @@ match os:
         # Npcap?
     case _:
         raise NotImplementedError("Unsupported or unknown operating system.")
+    
+# using scapy, open a capture handle on interface "en0"
+# apply "arp" filter -- access frame[Ether].type and check if it is equal to x0806 (ARP)
+# parse delivered packet, originally a buffer of raw bytes
+
+while(True):
+    packets = scapy.sniff(count=1)
+    current_frame = packets[0]
+    if current_frame["Ether"].type == 2054: # ARP Packet
+        print("ARP Packet found")
+        print(current_frame.summary)
+    elif current_frame["Ether"].type == 2048: # IPv4 packet
+        print("IPv4 Packet")
+        print(current_frame.summary)
+    time.sleep(0.5) # read a packet every half second
