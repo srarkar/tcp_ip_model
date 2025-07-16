5ea4b763897f80efb9973a0ff31c9a0ad05cb0e5
srarkar <rickdsarkar@gmail.com>
2025-07-16 19:19:09 -0400

    arp spoofing detection

--- data_link/arp_sniffer.py
+++ data_link/arp_sniffer.py
@@ -3,6 +3,7 @@
 
 import netifaces
 import scapy.all as scapy
+from scapy.all import Ether, ARP
 import platform
 import time
 from enum import Enum
@@ -28,20 +29,47 @@ match os:
     case _:
         raise NotImplementedError("Unsupported or unknown operating system.")
     
+    
 # using scapy, open a capture handle on interface "en0"
 # apply "arp" filter -- access frame[Ether].type and check if it is equal to x0806 (ARP)
 # parse delivered packet, originally a buffer of raw bytes
-
+ip_to_mac = {}
 while(True):
     packets = scapy.sniff(count=1)
     current_frame = packets[0]
     if current_frame["Ether"].type == EtherType.ARP.value: # ARP Packet
-        print("ARP Packet")
-        print(f"Sender MAC Address: {current_frame['ARP'].hwsrc}")
-        print(f"Sender IP Address: {current_frame['ARP'].psrc}")
-        break
+
+        # Access fields and place into locals
+        sender_mac_addr = current_frame['ARP'].hwsrc
+        sender_ip_addr = current_frame['ARP'].psrc
+
+        # dest_mac_addr = 
+        # dest_ip_addr = 
+
+
+        print("ARP Packet Found")
+        print(f"Sender IP Address: {sender_ip_addr}")
+        print(f"Sender MAC Address: {sender_mac_addr}")
+        if sender_ip_addr not in ip_to_mac:
+            ip_to_mac[sender_ip_addr] = {sender_mac_addr}
+        else:
+            if sender_mac_addr not in ip_to_mac[sender_ip_addr]:
+                ip_to_mac[sender_ip_addr].add(sender_mac_addr)
+                print(f"Multiple MAC addresses have been detected as mapped to the same IP.")
+                print(f"For IP address {sender_ip_addr}, the following MAC address mappings have been detected:")
+                for hwsrc in ip_to_mac[sender_ip_addr]:
+                    print("\t" + hwsrc)
+            
+                affirmatives = {"yes", "ye", "yurr", "yeah", "indeed"}
+                print(f"Continue sniffing?")
+                check_continue = input()
+                if check_continue in affirmatives:
+                    continue
+                else:
+                    break
+
+        time.sleep(0.25)
     elif current_frame["Ether"].type == EtherType.IPv4.value: # IPv4 packet
-        pass
-        # print("IPv4 Packet")
+        print("IPv4 Packet Found")
         # print(current_frame)
     # time.sleep(0.5) # read a packet every half second
