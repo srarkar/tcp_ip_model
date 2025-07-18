432070ee8596f16c8ef1b52fb4066dafde46c916
srarkar <rickdsarkar@gmail.com>
2025-07-17 22:30:19 -0400

    keyboard interrupt wih newline

--- data_link/arp_sniffer.py
+++ data_link/arp_sniffer.py
@@ -5,19 +5,30 @@ import netifaces
 import scapy.all as scapy
 from scapy.all import Ether, ARP
 import platform
+
 import time
+import sys
+import select
 from enum import Enum
 
 class EtherType(Enum):
     IPv4 = 2048
     ARP = 2054
 
-def print_report(num_arp_packets, num_ip_packets):
+def print_report(num_arp_packets, num_ip_packets, malicious_ips, malicious_macs):
     print(f"Number of packets sniffed: {num_arp_packets + num_ip_packets}")
     print(f"Number of ARP Packets: {num_arp_packets}")
     print(f"Number of IPv4 Packets: {num_ip_packets}")
-    print(f"Potential ARP Spoofing at the following IP Addresses: {list(malicious_ip)}")
+    print(f"Potential ARP Spoofing at the following IP Addresses: {list(malicious_ips)}")
     print(f"Potential IP Spoofing at the MAC Addresses: ")
+
+def detect_enter_keypress():
+    if sys.stdin in select.select([sys.stdin], [], [], 0)[0]:
+        line = sys.stdin.readline()
+        if line.strip() == "":
+            return True
+    return False
+
 
 interfaces = netifaces.interfaces()
 
@@ -34,6 +45,7 @@ match os:
         iface = "Ethernet"
     case _:
         raise NotImplementedError("Unsupported or unknown operating system.")
+
     
     
 # using scapy, open a capture handle on interface "en0"
@@ -42,12 +54,18 @@ match os:
 ip_to_mac = {}
 mac_to_ip = {}
 
-malicious_ip = {}
-malicious_mac = {}
+malicious_ip = set()
+malicious_mac = set()
 
 num_arp_packets = 0
 num_ip_packets = 0
+
+print("Commencing network sniffing...")
+print("Press ENTER at any point to terminate execution")
+
 while(True):
+    if detect_enter_keypress():
+        break
     packets = scapy.sniff(count=1, iface = iface)
     current_frame = packets[0]
     if current_frame["Ether"].type == EtherType.ARP.value: # ARP Packet
@@ -59,13 +77,16 @@ while(True):
         # dest_mac_addr = 
         # dest_ip_addr = 
 
-
+        # print fields
         print("ARP Packet Found")
         print(f"Sender IP Address: {sender_ip_addr}")
         print(f"Sender MAC Address: {sender_mac_addr}")
+
+        # maintain dictionary of ip addrs mapped to MAC addresses
         if sender_ip_addr not in ip_to_mac:
             ip_to_mac[sender_ip_addr] = {sender_mac_addr}
         else:
+            # found multiple mac addresses mapped to the same IP, possible ARP spoofing
             if sender_mac_addr not in ip_to_mac[sender_ip_addr]:
                 ip_to_mac[sender_ip_addr].add(sender_mac_addr)
                 print(f"Multiple MAC addresses have been detected as mapped to the same IP.")
@@ -75,12 +96,24 @@ while(True):
 
                 malicious_ip.add(sender_ip_addr)
 
-                affirmatives = {"yes", "ye", 'y', "yurr", "yeah", "indeed"}
+
+                # at this point, we've found a potential case of ARP Spoofing
+                # provide user choice to continue sniffing or break early
+                affirmatives = {"yes", "ye", 'y', "yurr", "yeah", "yup", "indeed"}
+                negatives = {"no", "n", "no thanks", "naw", "nope", "stop", "quit"}
                 print(f"Continue sniffing?")
-                check_continue = input()
-                if check_continue in affirmatives:
-                    continue
-                else:
+                exit_now = False
+                while(True):
+                    check_continue = input()
+                    if check_continue in affirmatives:
+                        break
+                    elif check_continue in negatives:
+                        exit_now = True
+                        break
+                    else:
+                        print("Please answer yes or no.")
+
+                if exit_now: 
                     break
     elif current_frame["Ether"].type == EtherType.IPv4.value: # IPv4 packet
         num_ip_packets += 1
