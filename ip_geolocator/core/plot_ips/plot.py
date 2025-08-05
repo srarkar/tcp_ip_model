319d0310017e8b97f0bdf90dd5de96f12832fa4b
srarkar <rickdsarkar@gmail.com>
2025-08-04 20:17:10 -0400

    stole google maps marker + fixed icon size bugs

--- ip_geolocator/core/plot_ips/plot.py
+++ ip_geolocator/core/plot_ips/plot.py
@@ -1,14 +1,15 @@
 import sys
 import folium
+from folium import Map, Marker, Icon, CustomIcon
 
 from pathlib import Path
-
+DEFAULT_SIZE = 30 # This is where the default size is set. 28 is a normal size and works ok but any larger causes problems
 ## TODO: add connections between sender and destination if settings[mapping] is True
 
 # print(f"Map saved to {save_path}")
 def get_icon_size(frequency, total):
     ratio = float(frequency + total) / total
-    return (28 * ratio, 28 * ratio)
+    return (DEFAULT_SIZE * ratio, DEFAULT_SIZE * ratio)
 
 def get_save_path(output_file):
     # Get the project root (parent of 'core')
@@ -21,10 +22,22 @@ def get_save_path(output_file):
     save_path = output_dir / output_file
     return save_path
 
+marker_path = Path(__file__).resolve().parents[2] / "utils" / "images" / "map_marker.png"
+if not marker_path.exists():
+    print(f"Error: Marker image not found at {marker_path}")
+    sys.exit(1)
+
 def get_marker(ip_request, icon_size):
-    return folium.Marker(icon=folium.Icon(icon="info-sign", icon_size=icon_size), location=[ip_request.lat, ip_request.long],
-            popup=f"{ip_request.ip_addr} - {ip_request.city}, {ip_request.country}",
-            tooltip=ip_request.ip_addr)
+    icon = CustomIcon(
+        icon_image=str(marker_path),
+        icon_size=icon_size
+    )
+    return folium.Marker(
+        location=[ip_request.lat, ip_request.long],
+        icon=icon,
+        popup=f"{ip_request.ip_addr}\n{ip_request.city}, {ip_request.country}",
+        tooltip=ip_request.ip_addr,
+    )
     
 
 def plot_ips_on_map(request_obj_lst, frequency_map, output_file = "ip_map.html"):
@@ -50,4 +63,5 @@ def plot_ips_on_map(request_obj_lst, frequency_map, output_file = "ip_map.html")
     ip_map.save(save_path)
     print(f"Map saved to {save_path}. Open it in your browser to view.")
 
-def plot_connections(ip_pairs)
+def plot_connections(ip_pairs):
+    pass
